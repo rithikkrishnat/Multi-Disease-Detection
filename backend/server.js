a270b41a738ac8d7ae5c2450607d11c69c60c3f5
. . <rithikkrishnat@gmail.com>
2026-02-15 21:22:20 +0530

    docker deployment

--- backend/server.js
+++ backend/server.js
@@ -57,7 +57,7 @@ app.post('/api/diagnose', upload.single('image'), (req, res) => {
     });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
